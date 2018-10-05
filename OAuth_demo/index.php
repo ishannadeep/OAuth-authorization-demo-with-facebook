8772018811ad7c3c54c2bf375716a0b35891fbd9
ishan <ishan@DESKTOP-IDDG36R>
2018-10-05 23:22:58 +0530

    fixed on load FB login window prompt issue

--- OAuth_demo/index.php
+++ OAuth_demo/index.php
@@ -15,6 +15,7 @@ and open the template in the editor.
     <body>
 
         <script>
+
             function setCookie(cname, cvalue) {
 
                 console.log("cookie set");
@@ -29,31 +30,28 @@ and open the template in the editor.
                     version: 'v3.1'
                 });
                 FB.AppEvents.logPageView();
-
-                FB.login(function (response) {
-                    if (response.authResponse) {
-
+                FB.getLoginStatus(function (response) {
+                    if (response.status === 'connected') {
                         var accessToken = response.authResponse.accessToken;
                         console.log("connected");
                         setCookie('token', accessToken);
                         window.top.location = "home.php";
-                    }
-                });
-                FB.getLoginStatus(function (response) {
-                    if (response.status === 'connected') {
+
                         console.log("connected");
                     } else if (response.status === 'authorization_expired') {
-
+                        FB.logout();
                         console.log("authorization_expired");
                     } else if (response.status === 'not_authorized') {
-
+                        FB.logout();
                         console.log("not_authorized");
                     } else {
-
                         console.log("notlogin");
                     }
                 });
             };
+            function red() {
+                location.reload();
+            }
             (function (d, s, id) {
                 var js, fjs = d.getElementsByTagName(s)[0];
                 if (d.getElementById(id)) {
@@ -89,17 +87,6 @@ and open the template in the editor.
             </form>
         </div>
         <div id="fb-root"></div>
-        <script>
-            (function (d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id))
-                    return;
-                js = d.createElement(s);
-                js.id = id;
-                js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1&appId=238515270159151&autoLogAppEvents=1';
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-        </script>
-        <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false" style="margin-left: 315px;"></div>
+        <div class="fb-login-button" onclick="" onlogin= "red()" id="fbbut" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="true" style="margin-left: 315px;"></div>
     </body>
 </html>
